d38eb5a8073d25bdde0edacbe2e3a7c20206799b
vahid sahray <vahidsahray@93gmail.com>
2025-11-24 02:08:39 +0330

    Sprint 2: improved admin panel and auto slug

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
+from django.utils.text import slugify
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "created_at", "status")
+    list_filter = ("status", "created_at")
+    search_fields = ("title", "content")
+    ordering = ("-created_at",)
--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,25 @@
 from django.db import models
+from django.utils.text import slugify
 from django.contrib.auth.models import User
 
 class Post(models.Model):
+
+    STATUS_CHOICES = (
+        ("draft", "Draft"),
+        ("published", "Published"),
+    )
+
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True)
-    body = models.TextField()
+    content = models.TextField()
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="draft")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
