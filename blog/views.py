f2fb55e802b046944a8787c67d4892a98444739a
vahid sahray <vahidsahray@93gmail.com>
2025-11-24 02:47:07 +0330

    Sprint 3:  basic blog views, urls, templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.filter(status="published").order_by("-created_at")
+    return render(request, "blog/post_list.html", {"posts": posts})
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug, status="published")
+    return render(request, "blog/post_detail.html", {"post": post})
+    
